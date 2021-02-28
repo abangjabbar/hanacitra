1af2a754602fc6feb55fe0ccd781868689d9a6de
@jabbar <jabbar@warungpintar.co>
2021-02-28 07:32:19 +0700

    forgot password menggunakan email

--- application/views/auth/forgot-password.php
+++ application/views/auth/forgot-password.php
@@ -12,30 +12,23 @@
                         <div class="col-lg">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Halaman Login!</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">Apakah anda lupa password anda?</h1>
                                 </div>
 
                                 <?= $this->session->flashdata('message'); ?>
 
-                                <form class="user" method="post" action="<?= site_url('auth'); ?>">
+                                <form class="user" method="post" action="<?= site_url('auth/forgotpassword'); ?>">
                                     <div class="form-group">
                                         <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email'); ?>">
                                         <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
-                                    </div>
                                     <button type="submit" class="btn btn-primary btn-user btn-block">
-                                        Login
+                                        Atur ulang password
                                     </button>
                                 </form>
                                 <hr>
                                 <div class="text-center">
-                                    <a class="small" href="<?= base_url('auth/forgotpassword') ?>">Forgot Password?</a>
-                                </div>
-                                <div class="text-center">
-                                    <a class="small" href="<?= site_url('auth/registration') ?>">Create an Account!</a>
+                                    <a class="small" href="<?= site_url('auth/registration') ?>">Kembali ke halaman login</a>
                                 </div>
                             </div>
                         </div>
